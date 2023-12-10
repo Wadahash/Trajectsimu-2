f9c776f26a46692f1a809795d40603b66ac912e4
Wadahash <115351129+Wadahash@users.noreply.github.com>
2023-12-10 21:56:42 +0900

    Update errors.py

--- scripts/errors.py
+++ scripts/errors.py
@@ -1,1 +1,10 @@
+# Error code classes
+
+class ParameterDefineError(Exception):
+    # error if parameter definition is incorrect
+    pass
+
+class CurveFittingError(Exception):
+    # error if parameter definition is incorrect
+    pass
 
